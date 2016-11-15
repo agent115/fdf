7ff83e03db875f9ec2d178ca219463e3a574649d
AGENT 115 <Agent1156@yandex.ru>
2016-11-15 13:02:52 +0300

    Первый коммит

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Кит энерго</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+
+<header>
+
+    <div class="container">
+        <div style="overflow: hidden;">
+            <div id="header" style="width: 1000%;">
+                <div id="logo" style="float: left; width: 300px; ">
+                    <img src="images/logo.png">
+                    <i>КИТ ЭНЕРГЕТИКА</i>
+                    <p>УПРАВЛЯЙ ЭНЕРГИЕЙ</p>
+                </div>
+
+                <div id="log-center"
+                ">
+                <h3><a class="hover" href="">Энергия </a> | <a class="hover" href=""> Вернуться в КИТ Энергеника</a>
+                </h3>
+            </div>
+
+            <div id="log-right">
+                <span><a href=""><img src="images/ss1.png" </a> </span>
+                <span><a href=""><img src="images/ss2.png" </a> </span>
+                <span><a href=""><img src="images/ss3.png" </a> </span>
+                <span><a href=""><img src="images/ss4.png" </a> </span>
+
+
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+</header>
+
+
+<section>
+    <div class="section">
+        <span class="selection"><h3>Введите потребление электрической энергии, кВт.ч.</h3></span>
+
+        <div class="contant">
+            <i><b>2015</b></i>
+        </div>
+
+        <div class="crug">
+            <i>ЯНВ</i>
+        </div>
+        <div class="crug">
+            <i>ФЕВ</i>
+        </div>
+        <div class="crug">
+            <i>МАР</i>
+        </div>
+        <div class="crug">
+            <i>АПР</i>
+        </div>
+        <div class="crug">
+            <i>МАЙ</i>
+        </div>
+        <div class="crug">
+            <i>ИЮН</i>
+        </div>
+        <div class="crug">
+            <i>ИЮЛ</i>
+        </div>
+        <div class="crug">
+            <i>АВГ</i>
+        </div>
+        <div class="crug">
+            <i>СЕН</i>
+        </div>
+        <div class="crug">
+            <i>ОКТ</i>
+        </div>
+        <div class="crug">
+            <i>НОЯ</i>
+        </div>
+
+        <div class="crug">
+            <i>ДЕК</i>
+        </div>
+
+        <div id="contant">
+            <i><b>2016</b></i>
+        </div>
+
+        <div class="crug-2">
+            <i>ЯНВ</i>
+        </div>
+        <div class="crug-2">
+            <i>ФЕВ</i>
+        </div>
+        <div class="crug-2">
+            <i>МАР</i>
+        </div>
+        <div class="crug-2">
+            <i>АПР</i>
+        </div>
+        <div class="crug-2">
+            <i>МАЙ</i>
+        </div>
+        <div class="crug-2">
+            <i>ИЮН</i>
+        </div>
+        <div class="crug-2">
+            <i>ИЮЛ</i>
+        </div>
+        <div class="crug-2">
+            <i>АВГ</i>
+        </div>
+        <div class="crug-2">
+            <i>СЕН</i>
+        </div>
+        <div class="crug-2">
+            <i>ОКТ</i>
+        </div>
+        <div class="crug-2">
+            <i>НОЯ</i>
+        </div>
+
+        <div class="crug-2">
+            <i>ДЕК</i>
+        </div>
+
+
+        <div id="regim">
+            <ul>
+                <li><a href="">Тепло</a></li>
+                <li><a href="">Электричество</a></li>
+
+                <li><a href="">Вода</a></li>
+
+                <li><a href="">Джоули</a></li>
+
+                <li><a href="">Топливо</a></li>
+                <li><a href="">CO <span id="co2">2</span></a></li>
+
+            </ul>
+        </div>
+    </div>
+
+    <div id="footer-section">
+
+    </div>
+</section>
+
+
+<footer class="container">
+    <div id="footer">
+        <h5><a href="" class="hover">FAQ </a>
+            <a href="" class="hover"> Поддержка</a></h5>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ Новая папка/index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Кит энерго</title>
+</head>
+<body>
+
+
+<header>
+
+    <div class="container">
+        <div style="overflow: hidden;">
+            <div id="header" style="width: 1000%;">
+                <div class="logo" style="float: left; width: 300px; ">
+                    <img src="images/logo.png" class="logo-img">
+                    <h3>КИТ ЭНЕРГЕТИКА</h3>
+                    <p>УПРАВЛЯЙ ЭНЕРГИЕЙ</p>
+                    <div class="log-center-dop"
+                    ">
+                    <h3><a class="hover" href="">Энергия </a> | <a class="hover" href=""> Вернуться в КИТ Энергеника</a>
+                    </h3></div>
+                </div>
+
+                <div class="log-center"
+                ">
+                <h3><a class="hover" href="">Энергия </a> | <a class="hover" href=""> Вернуться в КИТ Энергеника</a>
+                </h3></div>
+
+        <div class="log-right">
+            <a href="#"> <span id="sprait1" class="hover"></span></a>
+            <a href="#"> <span id="sprait2" class="hover"></span></a>
+            <a href="#"> <span id="sprait3" class="hover"></span></a>
+            <a href="#"> <span id="sprait4" class="hover"></span></a>
+            <a href="#"> <span id="sprait5" class="hover"></span></a>
+            <a href="#"> <span id="sprait6" class="hover"></span></a>
+
+        </div>
+    </div>
+    </div>
+
+
+    <div class="regims">
+        <ul>
+            <li><a href="">Тепло</a></li>
+            <li><a href="">Электричество</a></li>
+
+            <li><a href="">Вода</a></li>
+
+            <li><a href="">Джоули</a></li>
+
+            <li><a href="">Топливо</a></li>
+            <li><a href="">CO <span class="co2">2</span></a></li>
+
+        </ul>
+    </div>
+</header>
+
+
+<section>
+    <div class="section">
+        <span id="selection"><h3>Введите потребление электрической энергии, кВт.ч.</h3></span>
+        <span class="selection-1"><h3>2015</h3></span>
+
+        <div class="contant">
+            <i><b>2015</b></i>
+        </div>
+
+        <div class="crug padding-one">
+            <i>ЯНВ</i>
+        </div>
+        <div class="crug">
+            <i>ФЕВ</i>
+        </div>
+        <div class="crug">
+            <i>МАР</i>
+        </div>
+        <div class="crug">
+            <i>АПР</i>
+        </div>
+        <div class="crug">
+            <i>МАЙ</i>
+        </div>
+        <div class="crug">
+            <i>ИЮН</i>
+        </div>
+        <div class="crug padding-one">
+            <i>ИЮЛ</i>
+        </div>
+        <div class="crug">
+            <i>АВГ</i>
+        </div>
+        <div class="crug">
+            <i>СЕН</i>
+        </div>
+        <div class="crug">
+            <i>ОКТ</i>
+        </div>
+        <div class="crug">
+            <i>НОЯ</i>
+        </div>
+
+        <div class="crug">
+            <i class="ty">ДЕК</i>
+        </div>
+        <br/>
+
+
+        <span class="selection-2"><h3>2016</h3></span>
+
+        <div class="contant">
+            <i><b>2016</b></i>
+        </div>
+
+        <div class="crug-2 padding-one selection-2-padding">
+            <i>ЯНВ</i>
+        </div>
+        <div class="crug-2 selection-2-padding">
+            <i>ФЕВ</i>
+        </div>
+        <div class="crug-2 selection-2-padding">
+            <i>МАР</i>
+        </div>
+        <div class="crug-2 selection-2-padding">
+            <i>АПР</i>
+        </div>
+        <div class="crug-2 selection-2-padding">
+            <i>МАЙ</i>
+        </div>
+        <div class="crug-2 selection-2-padding">
+            <i>ИЮН</i>
+        </div>
+        <div class="crug-2 padding-one">
+            <i>ИЮЛ</i>
+        </div>
+        <div class="crug-2">
+            <i>АВГ</i>
+        </div>
+        <div class="crug-2">
+            <i>СЕН</i>
+        </div>
+        <div class="crug-2">
+            <i>ОКТ</i>
+        </div>
+        <div class="crug-2">
+            <i>НОЯ</i>
+        </div>
+
+        <div class="crug-2">
+            <i class="ty">ДЕК</i>
+        </div>
+
+
+
+
+        <div class="regim">
+            <ul>
+                <li><a href="">Тепло</a></li>
+                <li><a href="">Электричество</a></li>
+
+                <li><a href="">Вода</a></li>
+
+                <li><a href="">Джоули</a></li>
+
+                <li><a href="">Топливо</a></li>
+                <li><a href="">CO <span class="co2">2</span></a></li>
+
+            </ul>
+        </div>
+
+        <div class="bacg_left">
+            <img src="images/bacg_left.png">
+        </div>
+        <div class="bacg_rait">
+            <img src="images/bacg_rait.png">
+        </div>
+
+        <div class="blok-color">
+            <span class="cvadrat"><i>2015г</i></span>
+            <span class="cvadrat-2"><i>2016г</i></span>
+        </div>
+
+    </div>
+
+    <div class="footer-section">
+
+    </div>
+</section>
+
+
+<footer class="container">
+    <div class="footer">
+        <h5><a href="" class="hover">FAQ </a>
+            <a href="" class="hover"> Поддержка</a></h5>
+            <span class="footer-ss">
+            <a href="#"> <span id="sprait1" class="hover"></span></a>
+            <a href="#"> <span id="sprait2" class="hover"></span></a>
+            <a href="#"> <span id="sprait3" class="hover"></span></a>
+            <a href="#"> <span id="sprait4" class="hover"></span></a>
+            <a href="#"> <span id="sprait5" class="hover"></span></a>
+            <a href="#"> <span id="sprait6" class="hover"></span></a>
+</span>
+
+    </div>
+</footer>
+
+</body>
+</html>
